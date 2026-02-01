3312262bdf81d9fa2517c6e35d6b8fb5ca1bc4a6
stephen olise <stpholise@gmail.com>
2026-02-01 16:21:51 +0100

    update about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -36,13 +36,16 @@ const About = () => {
                 {" "}
                 Our Journey
               </h3>
-              <div className="flex flex-col gap-3">
+              <div className="flex flex-col gap-4">
                 {journey.map((item, index) => (
                   <div
-                    className="flex items-center gap-3 sm:flex-row flex-col"
+                    data-aos="fade-left"
+                    data-aos-duration="700"
+                    data-aos-once="true"
+                    className="flex sm:items-center gap-2 sm:gap-3 mb-1 sm:flex-row flex-col"
                     key={index}
                   >
-                    <p className="text-white bg-logo-blue font-medium px-2 py-1 text-[10px] w-8 flex items-center justify-center rounded-md">
+                    <p className="text-white bg-logo-blue font-medium px-4 border w-18 sm:px-2 py-1 text-3 sm:text-[10px] sm:w-8 flex items-center justify-center rounded-md">
                       {" "}
                       {item.year}
                     </p>
@@ -55,7 +58,7 @@ const About = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-col  w-full bg-logo-blue py-8 gap-4">
+      <div className="flex flex-col  w-full bg-logo-blue py-8  gap-4">
         <div className=" container mx-auto px-2 py-2 rounded-lg sm:py-4">
           <h5 className=" text-lg text-left font-medium mb-1 sm:mb-6 text-logo-light ">
             Our Values
@@ -64,8 +67,8 @@ const About = () => {
             <CarouselComponent items={feedbacksItems} />
           </div>
         </div>
-        <div className="flex flex-col sm:flex-row container mx-auto gap-4">
-          <div className="px-2 py-2 rounded-lg bg-white">
+        <div className="flex flex-col sm:flex-row container mx-auto gap-4 px-2">
+          <div className="px-4 py-4 rounded-lg bg-white">
             <h5 className="text-lg font-medium text-left text-logo-blue mb-1">
               {" "}
               Our Mission
@@ -77,7 +80,7 @@ const About = () => {
               and sustainability.
             </p>
           </div>
-          <div className="px-2 py-2 rounded-lg bg-white h-fit">
+          <div className="px-4 py-4 rounded-lg bg-white h-fit">
             <h5 className="text-lg font-medium text-left text-logo-blue mb-1">
               {" "}
               Our Goal
