d0010cedd3a17e755d989ca4103e7bcba9371239
stephen olise <stpholise@gmail.com>
2026-02-04 13:54:59 +0100

    add request form for home page

--- src/components/utils/ScrollToTop.tsx
+++
@@ -1,9 +0,0 @@
-// import { useEffect, DO_NOT_USE_OR_YOU_WILL_BE_FIRED_EXPERIMENTAL_FORM_ACTIONS } from "react"
-
-// const ScrollToTop = () => {
-//   return (
-//     <div>ScrollToTop</div>
-//   )
-// }
-
-// export default ScrollToTop
